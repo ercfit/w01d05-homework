f927a1393c01994f7d41cbaaae3a8d8a22fab770
Erica Bruenton <ercfit@gmail.com>
2018-06-19 12:00:25 -0600

    Closet cleaned, on to Functions & Ojects...almost there

--- js/answers.js
+++ js/answers.js
@@ -226,11 +226,113 @@
 // }
 // (isLittle(200));
 
-// ******Monkey in the Middle
+// Monkey in the Middle
 // Write an `if...else if...else` statement:
+//	console.log `little number` if the number 		entered is less than **5**.
+//  If the number entered is more than 10, log `	big number`.
 
-// 1. console.log `little number` if the number entered is less than **5**.
+// Otherwise, console.log "monkey". 
 
-// 2.  If the number entered is more than 10, log `big number`.
+// 1. 
+	// function isLittle(num){
+	// 	if (num < 5){
+	// 		console.log ("little number");
+	// 	}else if (num > 10){
+	// 			console.log("big number");
+	// 		}else{
+	// 			console.log("monkey");
+	// 		}
+	// 	}
+	// isLittle();
+ 
 
-// 3. Otherwise, console.log "monkey". 
+//What's in Your Closet?
+
+
+
+const kristynsCloset = [
+						  "left shoe",
+						  "cowboy boots",
+						  "right sock",
+						  "GA hoodie",
+						  "green pants",
+						  "yellow knit hat",
+						  "marshmallow peeps"
+						];
+
+const thomsCloset = [
+					  [// These are Thom's shirts
+					    "grey button-up",
+					    "dark grey button-up",
+					    "light blue button-up",
+					    "blue button-up",
+					  ],
+					  [// These are Thom's pants
+					    "grey jeans",
+					    "jeans",
+					    "PJs"
+					  ],
+					  [// Thom's accessories
+					    "wool mittens",
+					    "wool scarf",
+					    "raybans"
+					  ]
+					];
+	
+// 1. What's Kristyn wearing today? Using bracket notation to access items in `kristynsCloset`, log the sentence "Kristyn is rocking that " + *the third item in Kristyn's closet* + " today!" to the console.
+	//console.log(`Kristyn is rocking that ${kristynsCloset[2]} today!`);
+
+// 2. Kristyn can't find her left shoe. Remove this item from her closet and save it to a variable named `kristynShoe`.
+	//const kristynShoe = kristynsCloset.shift();
+
+// 3. Kristyn just bought some sweet shades! Add `"raybans"` to her closet **after `"yellow knit hat"`.**
+	//kristynsCloset.splice(5, 0, "raybans");
+	//console.log(kristynsCloset);
+
+// 4. Kristyn spilled coffee on her hat... modify this item to read `"stained knit hat"` instead of yellow.
+	// kristynsCloset[4] = "stained knit hat";
+	// console.log(kristynsCloset);
+
+// 5. Put together an outfit for Thom! Using **bracket notation**, access the first element in Thom's `shirts` array.
+	//console.log(thomsCloset[0,0][0]);
+// 6. In the same way, access one item from Thom's pants array.
+	//console.log(thomsCloset[0,1][0]);
+// 7. Access one item from Thom's accessories array.
+	//console.log(thomsCloset[0,2][0]);
+// 8. Log a sentence about what Thom's wearing. Example: `"Thom is looking fierce in a grey button-up, jeans and wool scarf!"`
+	//console.log(`Thom is looking fierce in a ${thomsCloset[0,0][0]}, ${thomsCloset[0,1][0]}, and ${thomsCloset[0,2][0]}!`);
+// 9. Get more specific about what kind of PJs Thom's wearing this winter. Modify the name of his PJ pants to `Footie Pajamas`.
+	//(thomsCloset[0,1][2] = "Footie Pajamas");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
